5764d089da58986c73049bbfd360159eeb262275
grzegorzrychlin <rychlin@student.wszib.edu.pl>
2022-06-14 16:34:06 +0200

    Utworzenie reporytorium.

---
+++ src/main/java/projekt/zaliczeniowy/prowizjakredytowa/ProwizjakredytowaApplication.java
@@ -0,0 +1,13 @@
+package projekt.zaliczeniowy.prowizjakredytowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProwizjakredytowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProwizjakredytowaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projekt/zaliczeniowy/prowizjakredytowa/ProwizjakredytowaApplicationTests.java
@@ -0,0 +1,13 @@
+package projekt.zaliczeniowy.prowizjakredytowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProwizjakredytowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
